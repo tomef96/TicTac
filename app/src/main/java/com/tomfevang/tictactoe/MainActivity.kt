63b8a233059b8533c0b87c43cc28766ae1a2e95b
tomef <tomef96@gmail.com>
2019-01-29 23:56:35 +0100

    base game done, stupid AI

--- app/src/main/java/com/tomfevang/tictactoe/MainActivity.kt
+++ app/src/main/java/com/tomfevang/tictactoe/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.tomfevang.tictactoe
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val playAI: Button = findViewById(R.id.btnPlayAI)
+        playAI.setOnClickListener { v: View? ->
+            val intent = Intent(this@MainActivity, GameActivity::class.java)
+            intent.putExtra("aiMode", true)
+            startActivity(intent)
+        }
+
+        val playFriend: Button = findViewById(R.id.btnPlayFriend)
+        playFriend.setOnClickListener{
+            val intent = Intent(this@MainActivity, GameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
